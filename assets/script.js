7de05a7f49cee92af8900cd64dcdf46d161bd067
Keiji O <meekunn@gmail.com>
2023-05-14 00:55:25 -0700

    added local storage functions for cuisine input, function to move to page2.html

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,7 @@ const Spoonacular_API_jen = 'c6c9bb9062a14ace88c599472838ee3f';
 const cuisineInputEl = document.querySelector("#cuisine-input");
 const sportInputEl = document.querySelector("#sport-input");
 const submitBtn = document.querySelector("submit-btn");
+const saveDisplay = document.querySelector("#saveDisplay");  //In HTML, need to implemented ul with id= saveDisplay.
 
 
 //Weekly Calendar page DOM
@@ -45,6 +46,53 @@ window.addEventListener("load", () => {
 
 //------------------Locate Storage functions(https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage)
 
+//below are functions to load, save, display, and use locally stored cuisine element
+var saveList = ""; //initial blank savelist at load. Array. store cuisine input.
+
+//load local storage
+function loadSaved() {
+  var saved = JSON.parse(localStorage.getItem("saved"));
+  console.log(saved);
+  if (saved !== null) {
+    saveList = saved;
+  }}
+
+//save input value
+function storeSave() {
+  saveList.push(cuisineInputEl.value);  //currently set to cuisine input. change if needed
+  localStorage.setItem("saved", JSON.stringify(saveList));
+  }
+
+//displays the local storage save content. generates li with buttons nested to make list of saved content.
+function displaySave() {
+  saveDisplay.innerHTML = ""; //wipe reviously loaded content
+  for (var i = 0; i < saveList.length; i++) { 
+    var save = saveList[i]; 
+
+    var li = document.createElement("li");
+    li.textContent = "";
+    li.setAttribute("saveValue", i);
+
+    var button = document.createElement("button");
+    button.textContent = save;
+
+    li.appendChild(button);
+    saveList.appendChild(li);
+  }
+}  
+
+//re-search using saved content.
+function reloadSave(event) {
+  var element = event.target;
+
+  if (element.matches("button") === true) {
+    var index = element.parentElement.getAttribute("saveValue");
+  console.log(index);
+  inputValue = saveList[index];
+  fetchRecipe(cuisine); //or other function to start cuisine search process.
+  }
+}
+
 //-----------------------Get locally stored data---------------------------------------------------
 
 //GET (one) recipe, return (one) recipe JOSON from localStorage
@@ -67,7 +115,14 @@ function setLocalRecipesData (recipe) {
 
 };
 
+//--------------------------HTML changing functions--------------------------------------------------------
 
+//moves to page2.html
+function moveHTML(){
+  var queryString = "./page2.html?q=" + cuisineInputEl.value;
+
+  location.assign(queryString);
+}
 
 //------------------------Recipes Related functions below-----------------------------------------------
 
