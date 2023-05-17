981947bb3b05024d0098c4173dddddbc9c1e943c
Marinah Tamura <marinahtam@gmail.com>
2023-05-17 12:19:29 -0700

    edit styling and local storage

--- assets/script2.js
+++ assets/script2.js
@@ -1,5 +1,12 @@
-//Recipe Detail page DOM
 
+
+//Recipe Detail page DOM
+document.addEventListener('DOMContentLoaded', function() {
+    // Hide the bottom section initially
+   //# filler for now
+    #.classList.add('hide');
+    #.classList.add('hide');
+  });
 
 //Global Var
 
@@ -14,6 +21,9 @@ var currentRecipesIndex = window.location;
 
 
 //----------------DOM functions and eventlistener functions-------------------------------------------
+let searchValue = localStorage.getItem('searchValue') || "";
+
+// Use the retrieved value in your code
 
 
 //eventlistener for btn ? 
@@ -25,7 +35,27 @@ saveButton.addEventListener('click', saveInformation);
 //------------------------Recipes Related functions below----
 
 //get from localstorage function()
+saveButton.addEventListener('click', function() {
+    const savedItems = JSON.parse(localStorage.getItem('save-details'));
 
+    const savedItemensConatainer = document.getElementById('savedItemsContainer');
+
+    savedItemensConatainer.innerHTML = ''
+
+    if (savedItems && savedItems.length > 0) {
+
+        savedItems.forEach(function(item) { 
+            const listItem = document.createElement('li');
+            listItem.textContent = item;
+            savedItemensConatainer.appendChild(listItem);
+        });
+        
+    } else {
+        const message = document.createElement('p');
+        message.textContent = 'No saved items.';
+        savedItemensConatainer.appenedChild(message);
+    }
+})
 
 //display to UI function()
 
